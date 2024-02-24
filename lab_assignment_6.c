5fa5be700efd0a2a126a02d18addbfc93d1844ae
caitees <caity.wallsmith@gmail.com>
2024-02-23 22:22:13 -0500

    [Finished assignment]

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,8 +1,30 @@
+//Caitlin Wallsmith
+//2.23.24
+//Lab 6
 #include <stdio.h>
 
+//Write an efficient recursive function that takes in a sorted array of numbers, two integers, low and high, representing indexes into the array, and another integer, value, and returns the index in the array where value is found in the array in between index low and high, inclusive. If value is NOT found in the array in between indexes low and high, inclusive, then the function should return -1.
 int search(int numbers[], int low, int high, int value) 
 {
-	return -1;
+  //if value is not found in the array return -1.
+  if (low > high) 
+  {
+    return -1;
+  }
+  //get middle index
+  int mid = (low + high) / 2;
+  //if value is found in the middle index
+  if (numbers[mid] == value) {
+    return mid;
+  }
+  //if middle index is less than value have the function search the right side of the array
+  else if (numbers[mid] < value) {
+    return search(numbers, mid + 1, high, value);
+  }
+  //if not, search left side of array
+  else {
+    return search(numbers, low, mid - 1, value);
+  }
 }
 
 void printArray(int numbers[], int sz)
